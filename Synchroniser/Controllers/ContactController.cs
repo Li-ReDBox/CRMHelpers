796cae8a0a5c6f283c72efc6e447049b706d0a54
Jianfeng Li <jianfeng.li@ersa.edu.au>
2018-02-23 16:17:45 +1030

    Use querystring not named action parameter

--- Synchroniser/Controllers/ContactController.cs
+++ Synchroniser/Controllers/ContactController.cs
@@ -65,13 +65,13 @@ namespace Synchroniser.Controllers
             return await Edit(new Guid(content.ID));
         }
 
-        public async Task<IActionResult> Search(string id)
+        public async Task<IActionResult> Search(string email)
         {
-            _logger.LogDebug($"ContactController::Search has been called with {id}");
-            if (!string.IsNullOrEmpty(id))
+            _logger.LogDebug($"ContactController::Search has been called with {email}");
+            if (!string.IsNullOrEmpty(email))
             {
-                _logger.LogDebug($"You are looking for the contact by {id}");
-                var result = await contact.GetByEmail(id);
+                _logger.LogDebug($"You are looking for the contact by {email}");
+                var result = await contact.GetByEmail(email);
                 if (result != null)
                 {
                     return Redirect(Url.Action("Get", new { id = new Guid(result.ID) }));
